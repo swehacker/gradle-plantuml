57d0c2fc678c6fd354ca544156af6d6436909a7f
Patrik Falk <patrik@patrikfalk.com>
2018-10-07 13:55:06 +0200

    Bumped the version to 0.1.0 and removed commented out code

--- src/main/java/swehacker/gradle/plugin/plantuml/PlantUmlPlugin.java
+++ src/main/java/swehacker/gradle/plugin/plantuml/PlantUmlPlugin.java
@@ -10,16 +10,6 @@ public class PlantUmlPlugin implements Plugin<Project> {
 
   @Override
   public void apply(Project project) {
-    project.getTasks().create("generateDomainModel", GenerateDomainModelTask.class, (task) -> {
-      //task.setDocumentationDir(project.getProperties().get("docsDir").toString());
-      //task.setOutputFile("domain.puml");
-        //project.getBuildscript().getConfigurations().stream().forEach(config -> config.forEach(System.out::println));
-      //project.getConfigurations().getByName("compileClasspath").forEach(file -> System.out.println(file.toString()));
-
-      //task.setClassPath(project.getConfigurations().getByName("compileClasspath"));
-      //task.setClassLoader(project.getClass().getClassLoader());
-
-      //project.getConfigurations().findByName("runtime").getFiles().forEach(file -> System.out.println(file.getAbsolutePath()));
-    });
+    project.getTasks().create("generateDomainModel", GenerateDomainModelTask.class, (task) -> { });
   }
 }
